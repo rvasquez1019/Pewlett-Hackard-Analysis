676c08fe605eb3ebaa3e896d79c4a172dd47c2db
rvasquez1019 <66176022+rvasquez1019@users.noreply.github.com>
2020-07-15 21:22:14 -0500

    Add files via upload

---
+++ Pewlett-Hackard-Analysis.sql
@@ -0,0 +1,14 @@
+create table testTable (col1 varchar (10), col2 varchar(12), col3 int);
+--CREATE TABLE testTaBle (col1 Varchar(10)) Sql is case insensitive
+commit; 
+
+select * from testTable; --reads
+
+--ANSI sql is the standard but it loosely followed.  Each fdms is a l-
+
+insert into testtable (col1,col2,col3) values ('a','b',1);
+
+select * from testTable;
+
+
+
