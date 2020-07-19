b3685cd3dac09fb9deac2ea060afb724e4c19c9b
rvasquez1019 <66176022+rvasquez1019@users.noreply.github.com>
2020-07-19 17:46:02 -0500

    Add files via upload

--- Queries/Challenge 7.sql
+++ Queries/Challenge 7.sql
@@ -242,3 +242,78 @@ ON (ri.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no)
 WHERE d.dept_name IN ('Sale', 'Development');
+
+--Challenge 7
+--Number of Retiring Employees
+
+SELECT 	ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date
+INTO ret_titles
+FROM current_emp AS ce
+	INNER JOIN titles AS ti
+		ON (ce.emp_no = ti.emp_no)
+ORDER BY ce.emp_no;
+
+SELECT * FROM ret_titles
+ORDER BY ret_titles.emp_no;
+
+SELECT * FROM ret_titles;
+
+-- Partition the data to show only most recent title per employee.
+
+SELECT 	emp_no,
+		first_name,
+		last_name,
+		to_date,
+		title
+INTO emp_titles
+FROM (SELECT emp_no,
+			 first_name,
+			 last_name,
+			 to_date,
+			 title, ROW_NUMBER() OVER
+		(PARTITION BY (emp_no)
+		ORDER BY to_date DESC) rn
+		FROM ret_titles
+		) tmp 
+WHERE rn = 1
+ORDER BY emp_no;
+
+SELECT * FROM emp_titles;
+
+-- Counting the number of employees per title
+
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM emp_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+
+-- Creating a list of employees for Mentorship Eligibility.
+
+SELECT	e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+	ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship;
+
+
